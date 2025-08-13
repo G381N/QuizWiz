b3c064acf6479762259d596f34ce652a7585b748
Gebin George <gebin.official@gmail.com>
2025-08-13 06:19:17 +0000

    How is the firebase linked to this application because in the env folder

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "quizwiz-kids",
-  "appId": "1:1071210484937:web:a30927b3611ba9adeee918",
-  "storageBucket": "quizwiz-kids.firebasestorage.app",
-  "apiKey": "AIzaSyCra2XFg49Oa7yRRYs1kTbrYrYa9Kabn2I",
-  "authDomain": "quizwiz-kids.firebaseapp.com",
-  "messagingSenderId": "1071210484937"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
